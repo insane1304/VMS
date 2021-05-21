93df3d75e1289336ad3520d9e43a7a30ef8b0f62
insane1304 <iit2019124@iiita.ac.in>
2021-05-21 11:40:07 +0530

    fixed link db again

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
-mongoose.connect("mongodb://localhost:27017/todo", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-// mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true, useUnifiedTopology: true });
+// mongoose.connect("mongodb://localhost:27017/todo", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true, useUnifiedTopology: true });
 
 mongoose.set("useCreateIndex", true);
 const connection = mongoose.connection;
